ea4f0145c74e5a2cc82f130bedb4642fb631bc56
89081314528 <89081314528@mail.ru>
2019-11-22 20:46:23 +0300

    22/11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,21 +13,34 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
-        list.add(5);
         System.out.println(list);
-        System.out.println(isNumberInList(list, 5));
+        System.out.println(isNumberInList(5, list));
+        System.out.println(isNumberInList(7, list));
+        System.out.println(isNumberInList(-1, list));
+        System.out.println(isNumberInList(10, list));
     }
 
-    public static int isNumberInList (List <Integer> list, int number) {
+    public static int isNumberInList(int guessedNumber, List<Integer> list) {
+        int lowerBorder = list.get(0);
+        int upperBorder = list.get(list.size() - 1);
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == number) {
-                return i;
+            int number = (lowerBorder + upperBorder) / 2;
+            if ((guessedNumber > upperBorder) || (guessedNumber < lowerBorder)) {
+                return -1;
+            } else if (number == guessedNumber) {
+                return list.indexOf(number);
+            } else if (number > guessedNumber) {
+                upperBorder = number;
+            } else if (number < guessedNumber) {
+                lowerBorder = number;
             }
         }
         return -1;
     }
 }
+
+
